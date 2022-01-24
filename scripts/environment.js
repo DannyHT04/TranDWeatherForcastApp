32898b858759a0243e17a8691ea568cfa3ae9c96
DannyHT04 <dannyhat1@gmail.com>
2022-01-23 22:20:53 -0800

    hi

---
+++ scripts/environment.js
@@ -0,0 +1,15 @@
+
+
+let isLive = true;
+
+let prod = {
+    apiKey: 'de11588e6dfc1510f946409a79d11581',
+    isLive: isLive
+}
+
+let dev = {
+    apiKey: 'de11588e6dfc1510f946409a79d11581',
+    isLive: !isLive
+}
+
+export {prod, dev}
